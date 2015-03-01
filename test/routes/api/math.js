b422c7a3ec3932c3aed6dc201f2c9dc0e73ff165
Nathan Abraham <nabraham@gmail.com>
2015-03-01 18:02:39 -0500

    add mocha tests

---
+++ test/routes/api/math.js
@@ -0,0 +1,44 @@
+var assert = require('assert')
+  , math = require('../../../lib-cov/routes/api/math');
+
+function mockRequest(n) {
+    return {params: {'n' : n}};
+}
+
+function mockResponse(validator, done) {
+    return { 
+        send: function(r) {
+            if (validator !== undefined) {
+                validator(r);
+            }
+
+            if (done !== undefined) {
+                done();
+            }
+        }
+    }
+}
+
+describe('failure conditions', function() {
+    it('should fail on non-positive integers', function() {
+        var f = function() {
+            math.fib_slow(mockRequest(0), mockResponse());
+        }
+        assert.throws(f, /fibbonacci defined/);
+    });
+
+    it('should fail on non-natural integers', function() {
+        var f = function() {
+            math.fib_slow(mockRequest('foo'), mockResponse());
+        }
+        assert.throws(f, /numeric argument/);
+    });
+});
+
+describe('regular use case', function() {
+    it('should return 144 for n=12', function(done) {
+        math.fib_slow(mockRequest(12), mockResponse(function(r) {
+            assert.equal(r.result, 144);
+        }, done));
+    });
+});
