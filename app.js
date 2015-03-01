bbcfe37105de21c9fb3c2ec87dc13e94b1fb5d07
Nathan Abraham <nabraham@gmail.com>
2015-03-01 14:39:40 -0500

    move routes into swagger

--- app.js
+++ app.js
@@ -1,26 +1,67 @@
-var express = require('express')
-  , math = require('./lib/routes/api/math');
+var bodyParser = require('body-parser')
+  , express = require('express')
+  , expressJson = require('express-json')
+  , math = require('./lib/routes/api/math')
+  , swagger_node = require('swagger-node-express')
+  , url = require('url');
 
 var app = express();
-initRoutes(app);
-startServer(app);
+app.use(expressJson());
+app.use(bodyParser.urlencoded());
 
-function initRoutes(app) {
-    app.get('/api/helloworld', function(req, res) {
-        res.send({hello: 'world'});
-    });
+var swagger = swagger_node.createNew(app);
+initRoutes(swagger);
+startServer(app, swagger);
 
-    app.get('/api/math/fib_fast/:n', math.fib_fast);
-    app.get('/api/math/fib_slow/:n', math.fib_slow);
+function initRoutes(swagger) {
+    var hello_world = {
+        spec: {
+            description : 'simple GET example',
+            path: '/api/helloworld',
+            summary: 'returns {"hello":"world"}',
+            paramters: [],
+            nickname: 'helloworld'
+        },
+        action: function(req, res) {
+            res.send({hello: 'world'});
+        }
+    };
+    
+    var fib_slow = {
+        spec: {
+            description: 'slow implementation of fibbonacci',
+            path: '/api/math/fib_slow/{n}',
+            notes: 'does not use dynamic programming',
+            summary: 'returns nth fibbonacci term',
+            parameters: [swagger.paramTypes.path('n', 'nth term to compute', 'Integer')],
+            nickname: 'fib_slow'
+        },
+        action: math.fib_slow
+    };
+
+    var fib_fast = {
+        spec: {
+            description: 'fast implementation of fibbonacci',
+            path: '/api/math/fib_fast/{n}',
+            notes: 'does not use dynamic programming',
+            summary: 'returns nth fibbonacci term',
+            parameters: [swagger.paramTypes.path('n', 'nth term to compute', 'Integer')],
+            nickname: 'fib_fast'
+        },
+        action: math.fib_fast
+    };
+
+    swagger.addGet(hello_world);
+    swagger.addGet(fib_slow);
+    swagger.addGet(fib_fast);
 }
 
-function startServer(app) {
+function startServer(app, swagger) {
+    swagger.configureSwaggerPaths("", "/docs", "");
+    swagger.configure('http://localhost:3000/docs','0.1');
     var server = app.listen(3000, function () {
-
         var host = server.address().address
         var port = server.address().port
-
         console.log('Example app listening at http://%s:%s', host, port)
-
     })
 }
