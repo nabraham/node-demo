8c6470263312ae5a08177e6b0798e1caa10f4573
Nathan Abraham <nabraham@gmail.com>
2015-03-01 22:14:18 -0500

    use cluster to take advantage of extra cpus

--- app.js
+++ app.js
@@ -1,36 +1,56 @@
 var bodyParser = require('body-parser')
+  , cluster = require('cluster')
   , express = require('express')
   , expressJson = require('express-json')
   , expressWinston = require('express-winston')
   , swagger_node = require('swagger-node-express')
   , swaggerResources = require('./swaggerResources.js')
   , url = require('url')
-  , winston = require('winston');
+  , winston = require('winston')
+  , _ = require('lodash');
 
-var app = express();
-app.use(expressJson());
-app.use(bodyParser.urlencoded());
-app.use('/swagger', express.static(__dirname + '/public/swagger'));
-app.use(expressWinston.logger({
-    transports: [
-        new winston.transports.Console({
-            colorize: true
-        })
-        //transports can be custom functions, built in, or user supported: 
-        //file, http, mongoDB, couchDB, mysql, loggly, redis, mail
-    ]
-}));
+if (cluster.isMaster) {
+    var nCpus = require('os').cpus().length;
+    console.log('Initializing app');
+    for (var i=0; i<_.min([4, nCpus]); i++) {
+        cluster.fork();
+    }
+    
+    cluster.on('exit', function(worker) {
+        console.log('Worker %d died; replacing', worker.id);
+        cluster.fork();
+    });
+} else {
+    startApp();    
+}
 
-var swagger = swagger_node.createNew(app);
-swaggerResources.addRoutes(swagger);
-startServer(app, swagger);
+function startApp() {
+    var app = express();
+    app.use(expressJson());
+    //app.use(bodyParser.urlencoded());
+    app.use('/swagger', express.static(__dirname + '/public/swagger'));
+    app.use(expressWinston.logger({
+        transports: [
+            new winston.transports.Console({
+                colorize: true
+            })
+            //transports can be custom functions, built in, or user supported: 
+            //file, http, mongoDB, couchDB, mysql, loggly, redis, mail
+        ]
+    }));
+
+    var swagger = swagger_node.createNew(app);
+    swaggerResources.addRoutes(swagger);
+    startServer(app, swagger);
+}
 
 function startServer(app, swagger) {
     swagger.configureSwaggerPaths("", "/docs", "");
     swagger.configure('http://localhost:3000/','0.1');
     var server = app.listen(3000, function () {
-        var host = server.address().address
-        var port = server.address().port
-        console.log('Example app listening at http://%s:%s', host, port)
-    })
+        var host = server.address().address;
+        var port = server.address().port;
+        console.log('Worker %d listening at http://%s:%s', 
+            cluster.worker.id, host, port);
+    });
 }
